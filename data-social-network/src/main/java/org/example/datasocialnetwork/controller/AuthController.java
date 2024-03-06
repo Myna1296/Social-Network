423d6df1a8345a1d37d54fbc418e894203aeefe8
Myna1296 <phannga1296@gmail.com>
2024-03-06 17:50:12 +0700

    update 20240306

--- data-social-network/src/main/java/org/example/datasocialnetwork/controller/AuthController.java
+++ data-social-network/src/main/java/org/example/datasocialnetwork/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
 
 import javax.validation.Valid;
 
@@ -13,8 +14,11 @@ import javax.validation.Valid;
 public class AuthController {
 
     @PostMapping("/register")
-    public void register(@Valid @RequestBody UserDTO userDTO){
-
+    public void register(@Valid @RequestBody UserDTO userDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            ResponseOk response = new Response();
+            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }
 
     }
 
---
+++ data-social-network/src/main/java/org/example/datasocialnetwork/dto/response/ResponseOk.java
@@ -0,0 +1,9 @@
+package com.example.websocialnetwork.dto.reponse;
+
+import lombok.Data;
+
+@Data
+public class ResponseOk {
+    private int code;
+    private String message;
+}
